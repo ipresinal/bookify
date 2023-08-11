d70bd8685d4f346c60441ca1282036d620fa6e5c
Ivan Presinal <ivan.presinal@hotmail.com>
2023-08-11 14:58:41 -0400

    finishing domain setup

---
+++ src/Bookify.Domain/Bookings/Booking.cs
@@ -0,0 +1,139 @@
+﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
+using Bookify.Domain.Bookings.Events;
+using Bookify.Domain.Shared;
+
+namespace Bookify.Domain.Bookings;
+
+public sealed class Booking : Entity
+{
+    private Booking(
+        Guid id,
+        Guid apartmentId,
+        Guid userId,
+        DateRange duration,
+        Money priceForPeriod,
+        Money cleaningFee,
+        Money amenitiesUpCharge,
+        Money totalPrice,
+        BookingStatus status,
+        DateTime createdOnUtc)
+    : base(id)
+    {
+        ApartmentId = apartmentId;
+        UserId = userId;
+        Duration = duration;
+        PriceForPeriod = priceForPeriod;
+        CleaningFee = cleaningFee;
+        AmenitiesUpCharge = amenitiesUpCharge;
+        TotalPrice = totalPrice;
+        Status = status;
+        CreatedOnUtc = createdOnUtc;
+
+    }
+
+    public Guid ApartmentId { get; private set; }
+
+    public Guid UserId { get; private set; }
+
+    public DateRange Duration { get; private set; }
+
+    public Money PriceForPeriod { get; private set; }
+
+    public Money CleaningFee { get; private set; }
+
+    public Money AmenitiesUpCharge { get; private set; }
+
+    public Money TotalPrice { get; private set; }
+
+    public BookingStatus Status { get; private set; }
+
+    public DateTime CreatedOnUtc { get; private set; }
+
+    public DateTime? ConfirmedOnUtc { get; private set; }
+
+    public DateTime? RejectedOnUtc { get; private set; }
+
+    public DateTime? CompletedOnUtc { get; private set; }
+
+    public DateTime? CancelledOnUtc { get; private set; }
+
+    public static Booking Reserve(
+        Apartment apartment,
+        Guid userId,
+        DateRange duration,
+        DateTime utcNow,
+        PricingDetails pricingDetails)
+    {
+        var booking = new Booking(
+            Guid.NewGuid(),
+            apartment.Id,
+            userId,
+            duration,
+            pricingDetails.PriceForPeriod,
+            pricingDetails.CleaningFee,
+            pricingDetails.AmenitiesUpCharge,
+            pricingDetails.TotalPrice,
+            BookingStatus.Reserved,
+            utcNow);
+
+        booking.RaiseDomainEvent(new BookingReservedDomainEvent(booking.Id));
+
+        apartment.LastBookedOnUtc = utcNow;
+
+        return booking;
+    }
+
+    public IAsyncResult Confirm(DateTime utcNow)
+    {
+        if (Status != BookingStatus.Reserved)
+        {
+            return IAsyncResult.Failure(BookingErrors.NotPending);
+        }
+
+        Status = BookingStatus.Confirmed;
+        ConfirmedOnUtc = utcNow;
+
+        RaiseDomainEvent(new BookingConfirmedDomainEvent(Id));
+
+        return IAsyncResult.Success();
+    }
+
+    public IAsyncResult Reject(DateTime utcNow)
+    {
+        if (Status != BookingStatus.Reserved)
+        {
+            return IAsyncResult.Failure(BookingErrors.NotPending);
+        }
+
+        Status = BookingStatus.Rejected;
+        RejectedOnUtc = utcNow;
+
+        RaiseDomainEvent(new BookingRejectedDomainEvent(Id));
+
+        return IAsyncResult.Success();
+    }
+
+    public IAsyncResult Cancel(DateTime utcNow)
+    {
+        if (Status != BookingStatus.Reserved)
+        {
+            return IAsyncResult.Failure(BookingErrors.NotConfirmed);
+        }
+
+        var currentdate = DateOnly.FromDateTime(utcNow);
+
+        if (currentdate > Duration.Start)
+        {
+            return IAsyncResult.Failure(BookingErrors.AlreadyStarted);
+        }
+
+        Status = BookingStatus.Cancelled;
+        CancelledOnUtc = utcNow;
+
+        RaiseDomainEvent(new BookingCancelledDomainEvent(Id));
+
+        return IAsyncResult.Success();
+    }
+
+}
---
+++ src/Bookify.Domain/Shared/Currency.cs
@@ -0,0 +1,39 @@
+﻿namespace Bookify.Domain.Shared;
+
+public record Currency
+{
+    // Available currencies
+    internal static readonly Currency None = new("");
+    public static readonly Currency Usd = new("USD");
+    public static readonly Currency Eur = new("EUR");
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="code"></param>
+    private Currency(string code) => Code = code;
+
+
+    public string Code { get; init; }
+
+    /// <summary>
+    /// Get a currency object from string code reference
+    /// </summary>
+    /// <param name="code"></param>
+    /// <returns></returns>
+    public static Currency FromCode(string code)
+    {
+        return All.FirstOrDefault(c => c.Code == code) ??
+               throw new ApplicationException("The currency code is invalid.");
+    }
+
+    /// <summary>
+    /// Shows to external the list of available currencies
+    /// </summary>
+    public static readonly IReadOnlyCollection<Currency> All = new[]
+    {
+        Usd,
+        Eur
+    };
+
+};
